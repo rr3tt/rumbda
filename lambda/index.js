42fc84ee172023d9eb26aff94b391f1caa210724
Michael Berkowitz <michael.berkowitz@gmail.com>
2017-06-08 15:28:35 -0400

    Process each record individually

--- lambda/index.js
+++ lambda/index.js
@@ -1,18 +1,21 @@
 const exec = require('child_process').exec;
 
 exports.handler = function(event, context) {
-  const child = exec('./ruby_wrapper ' + "'" +  JSON.stringify(event) + "' '" + JSON.stringify(context) + "'", (result) => {
-    context.done(result);
-  });
-
-  child.stdout.on('data', function (data) {
-    data.split("\n").forEach(function(x) {
-      console.log(x);
+  var context = context;
+  event['Records'].forEach(function(record) {
+    const child = exec('./ruby_wrapper ' + "'" +  JSON.stringify(record) + "' '" + JSON.stringify(context) + "'", (result) => {
+      context.done(result);
     });
-  });
-  child.stderr.on('data', function (data) {
-    data.split("\n").forEach(function(x) {
-      console.log(x);
+
+    child.stdout.on('data', function (data) {
+      data.split("\n").forEach(function(x) {
+        console.log(x);
+      });
+    });
+    child.stderr.on('data', function (data) {
+      data.split("\n").forEach(function(x) {
+        console.log(x);
+      });
     });
   });
 };
