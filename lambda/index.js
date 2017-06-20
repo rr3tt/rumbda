93f43af2c9ddd811728598d03a8239ecd6ed2c58
Michael Berkowitz <michael.berkowitz@gmail.com>
2017-06-20 13:56:22 -0400

    Look for chunk size in environment first

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,7 @@
 const execFile = require('child_process').execFile;
 
 exports.handler = function(event, context) {
-  const chunkSize = 10;
+  const chunkSize = process.env.CHUNK_SIZE || 10;
 
   var records = event.Records;
   var context = context;
