938ae50252a7339b9c0b71a1b8e05b53eea3674f
Brad Reynolds <bradleyjames@users.noreply.github.com>
2017-08-23 11:07:59 -0500

    Allow Rumbda Lambda to return JSON for API Gateway (#4)
    * Allow Rumbda to be called by AWS API Gateway.
    
    * Missed checking for error before processing the result.
    
    * Cleaned up the logic a bit.
    
    * Moved file path from event to an environment variable.

--- lambda/index.js
+++ lambda/index.js
@@ -1,10 +1,33 @@
 const exec = require('child_process').exec;
+const fs = require('fs');
 
-exports.handler = function(event, context) {
-  const child = exec('./ruby_wrapper ' + "'" +  JSON.stringify(event) + "'", (result) => {
-    context.done(result);
+exports.handler = function(event, context, callback) {
+  // In order to return a value and not deal with stdout rumbda watches a file.
+  const jsonFilePath = '/tmp/rumbda.' + Math.random() + '.json';
+  process.env['RUMBDA_RESULT_JSON_FILENAME'] = jsonFilePath;
+
+  const child = exec('./ruby_wrapper ' + "'" +  JSON.stringify(event) + "'", (error, stdout, stderr) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+
+    if (fs.existsSync(jsonFilePath)) {
+      fs.readFile(jsonFilePath, 'utf8', function (err, data) {
+        fs.unlink(jsonFilePath); 
+
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null, (data.length > 0) ? JSON.parse(data) : null);
+      });
+    } else {
+      callback(null);
+    }
   });
 
   child.stdout.on('data', console.log);
   child.stderr.on('data', console.error);
 };
+
